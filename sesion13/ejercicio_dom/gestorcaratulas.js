e6da2125133b8f58b2cf57a9bd189a9b4408da84
fpaniaguajavascript <fernando.paniagua.javascript@gmail.com>
2024-01-26 20:59:06 +0100

    Sesion 13

--- sesion13/ejercicio_dom/gestorcaratulas.js
+++ sesion13/ejercicio_dom/gestorcaratulas.js
@@ -1,12 +1,15 @@
 //const URL = "https://demo6481436.mockable.io/caratulas";
-const URL = "http://localhost:8080/gameboy_covers.json";
+const URL = "https://fpaniaguaangular.github.io/gamecovers/assets/gamecovers/covers_gameboy/gameboy_covers.json";
+
+var juegos;
 
 function getJSONFromURL(url) {
     var xmlHttp = new XMLHttpRequest();
     xmlHttp.onreadystatechange = function () {
         if (xmlHttp.readyState == 4) {
             if (xmlHttp.status == 200) {
-                procesarJuegos(JSON.parse(xmlHttp.responseText));
+                juegos = JSON.parse(xmlHttp.responseText);
+                procesarJuegos(juegos);
             } else {
                 console.error("Ha ocurrido un error:" + xmlHttp.status);
                 throw "Ha ocurrido un error " + xmlHttp.status;
@@ -25,7 +28,7 @@ function generarFicha(juego){
     const URL_IMAGEN = "https://fpaniaguaangular.github.io/gamecovers/assets/gamecovers/covers_gameboy/";
     let articulo = document.createElement("article");
     articulo.setAttribute('class','vj');//Asignamos un atributo class con valor vj
-    let titulo = document.createElement("h1");
+    let titulo = document.createElement("h3");
     let textoTitulo = document.createTextNode(juego.title);
     let imagen = document.createElement("img");
     imagen.src = URL_IMAGEN+juego.image;
@@ -34,4 +37,15 @@ function generarFicha(juego){
     articulo.appendChild(imagen);
     document.querySelector("#main").appendChild(articulo);
 }
+
+
+document.querySelector("#iBuscador").addEventListener("keydown",()=>{
+    document.querySelector("#main").innerHTML="";
+    let textoBuscado = document.querySelector("#iBuscador").value.trim().toUpperCase();
+    let juegosFiltrados = juegos.filter((juego)=>{
+        return juego.title.toUpperCase().includes(textoBuscado);
+    });
+    procesarJuegos(juegosFiltrados);
+});
+
 getJSONFromURL(URL);
